d1b7c4c2089bb98ccb9d8afc6a464e684987f160
idant1210 <85685239+idant1210@users.noreply.github.com>
2021-07-05 21:38:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,15 @@ import matplotlib.pyplot as plt
 import math
 import seaborn as sns
 from collections import Counter
-from sklearn.decomposition import PCA
+import sklearn
+from sklearn import preprocessing, linear_model, model_selection
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+
+plt.rcParams.update({'figure.max_open_warning': 0})
 
 date = []
 new_cases = []
@@ -46,29 +52,38 @@ for index,row in complete_dataset.iterrows():
 updated_df = pd.DataFrame({"location": location,"date":date,"new cases":new_cases,"total cases":total_cases,"new vaccined":new_vaccined,"total vaccined":total_vaccined,"population":population,"precent of population":vaccained_of_pop,"new cases of population":ncases_of_pop})
 updated_df = updated_df.fillna(0.0)
 updated_df['date'] = pd.to_datetime(updated_df['date'])
-updated_df = updated_df[~(updated_df['date'] < '2021-01-01')]
-updated_df = updated_df[~(updated_df['date'] > '2021-05-01')]
-updated_df = updated_df[updated_df['location'] != 'Gibraltar']
-updated_df = updated_df[updated_df['location'] != 'Falkland Islands']
-updated_df = updated_df[updated_df['location'] != 'Saint Helena']
-updated_df = updated_df[updated_df['precent of population'] != -1]
-updated_df = updated_df[updated_df['new cases of population'] != -1]
-
-top_8_vac = updated_df.drop_duplicates('location',keep='last').nlargest(8, ['precent of population'])
-top_8_total = updated_df.where(updated_df['location'].isin(top_8_vac['location'])).dropna()
-fig, axs = plt.subplots(2)
-fig.suptitle('Corona Virus vaccine effects in top 8 countrys')
-
-i = 0
-axs[0].set_title("Precent of vaccained out of total population")
-axs[1].set_title("Precent of new cases out of total population")
-for location in (top_8_vac['location']):
-	axs[0].plot(top_8_total['date'].where(top_8_total['location'] == location).dropna() ,top_8_total['precent of population'].where(top_8_total['location'] == location).dropna(),linewidth=2.0,label = location , linestyle = "-")
+new_df = (updated_df.where(updated_df['total vaccined'] != 0)).dropna()
+day_count = pd.DataFrame({"location": new_df['location'],"day count": int(0),"new cases":new_df['new cases'],"total cases":new_df['total cases'],"new vaccined":new_df['new vaccined'],"total vaccined":new_df['total vaccined'],"population":new_df['population'],"precent of population":new_df['precent of population'],"new cases of population":new_df['new cases of population']})
+for location in day_count['location'].drop_duplicates():
+	i=1
+	for index, row in (day_count.where(day_count['location']==location).dropna()).iterrows():
+		day_count.at[index,['day count']] = i
+		i = i+1
+i=0
+'''
+for location in day_count.drop_duplicates('location')['location']:
+	fig, axs = plt.subplots(2)
+	axs[0].plot(day_count['day count'].where(day_count['location'] == location).dropna() ,day_count['precent of population'].where(day_count['location'] == location).dropna(),linewidth=2.0,label = location , linestyle = "-", color = "#FF0000")
+	axs[1].plot(day_count['day count'].where(day_count['location'] == location).dropna() ,day_count['new cases of population'].where(day_count['location'] == location).dropna(),linewidth=2.0,label = location , linestyle = "-", color = "#4400FF")
+	axs[0].legend()
+	axs[1].legend()
+	fig.savefig('./countrys/'+location+'.png', bbox_inches='tight')
+'''
+######
+#Mechine learning
+######
+for location in day_count['location'].drop_duplicates():
 	i = i+1
-i = 0
-for location in (top_8_vac['location']):
-	axs[1].plot(top_8_total['date'].where(top_8_total['location'] == location).dropna() ,top_8_total['new cases of population'].where(top_8_total['location'] == location).dropna(),linewidth=2.0,label = location , linestyle = "-")
-	i = i+1
-axs[0].legend()
-axs[1].legend()
-plt.show()
+	for index, row in (day_count.where(day_count['location']==location).dropna()).iterrows():
+		day_count.at[index,['location']] = i
+y = day_count['new cases']
+del day_count['new cases']
+X_train, X_test, y_train, y_test = train_test_split(day_count, y, test_size = 0.3 , random_state = 41)
+minmax_scaler = preprocessing.MinMaxScaler().fit(X_train)
+X_train_scaled_minmax = minmax_scaler.transform (X_train)
+standard_scaler = preprocessing.StandardScaler().fit(X_train)
+X_train_scaled_standard = standard_scaler.transform (X_train)
+logistic_reg = LogisticRegression()
+classification_model_minmax = logistic_reg.fit(X_train_scaled_minmax,y_train)
+classification_model_standard = logistic_reg.fit(X_train_scaled_standard,y_train)
+print(classification_model_standard)
