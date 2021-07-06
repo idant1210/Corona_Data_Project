abf6ee828704162e909539663f1db0b0ee27eb29
idant1210 <85685239+idant1210@users.noreply.github.com>
2021-07-06 21:31:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 import math
 import seaborn as sns
 from collections import Counter
-from sklearn.decomposition import PCA
+import sklearn
+from sklearn import preprocessing, linear_model, model_selection
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
-sampled_coutrys = ["Israel","United States","United Kindom"]
+plt.rcParams.update({'figure.max_open_warning': 0})
+
 date = []
 new_cases = []
 total_cases = []
@@ -16,28 +24,71 @@ new_vaccined = []
 total_vaccined = []
 population = []
 location = []
+vaccained_of_pop = []
+ncases_of_pop = []
 
 complete_dataset = pd.read_csv("owid-covid-data.csv")
-
+complete_dataset.dropna()
 for index,row in complete_dataset.iterrows():
-	if row['location'] in sampled_coutrys:
-		date.append(row['date'])
-		new_cases.append(row['new_cases'])
-		total_cases.append(row['total_cases'])
-		new_vaccined.append(row['new_vaccinations'])
-		total_vaccined.append(row['people_fully_vaccinated'])
-		location.append(row['location'])
-		population.append(row['population'])
-updated_df = pd.DataFrame({"location": location,"date":date,"new cases":new_cases,"total cases":total_cases,"new vaccined":new_vaccined,"total vaccined":total_vaccined,"population":population})
-updated_df = updated_df.fillna(0)
-pre_veccaine = pd.DataFrame(updated_df.loc[updated_df['total vaccined'] < 1])
-vaccained = pd.DataFrame(updated_df.loc[updated_df['total vaccined'] > 0])
-usa_pre_df = pd.DataFrame(pre_veccaine.loc[pre_veccaine['location']=="United States"])
-usa_pre_df.drop_duplicates()
-usa_pre_df.to_csv("test")
-fig = plt.figure(figsize=(20,20))
-ax = fig.add_subplot(111)
-ax.plot(pre_veccaine['date'],pre_veccaine['total cases'].where(pre_veccaine['location']=="Israel"),color='lightblue', linewidth=2)
-ax.plot(usa_pre_df['date'],usa_pre_df['total cases'],color='red', linewidth=2)
-plt.show()
+	date.append(row['date'])
+	new_cases.append(row['new_cases'])
+	total_cases.append(row['total_cases'])
+	new_vaccined.append(row['new_vaccinations'])
+	total_vaccined.append(row['people_fully_vaccinated'])
+	location.append(row['location'])
+	population.append(row['population'])
+	try:
+		precent = (int(row['people_fully_vaccinated']) / int(row['population'])*100)
+	except ValueError:
+		precent= -1
+	vaccained_of_pop.append(precent)
+	try:
+		precent = (int(row['new_cases']) / int(row['population'])*100)
+	except ValueError:
+		precent = -1
+	ncases_of_pop.append(precent)
 
+#######
+#Dataset handeling and spliting to pre vaccaine and after vaccaine
+#######
+updated_df = pd.DataFrame({"location": location,"date":date,"new cases":new_cases,"total cases":total_cases,"new vaccined":new_vaccined,"total vaccined":total_vaccined,"population":population,"precent of population":vaccained_of_pop,"new cases of population":ncases_of_pop})
+updated_df = updated_df.fillna(0.0)
+updated_df['date'] = pd.to_datetime(updated_df['date'])
+new_df = (updated_df.where(updated_df['total vaccined'] != 0)).dropna()
+day_count = pd.DataFrame({"location": new_df['location'],"day count": int(0),"new cases":new_df['new cases'],"total cases":new_df['total cases'],"new vaccined":new_df['new vaccined'],"total vaccined":new_df['total vaccined'],"population":new_df['population'],"precent of population":new_df['precent of population'],"new cases of population":new_df['new cases of population']})
+for location in day_count['location'].drop_duplicates():
+	i=1
+	for index, row in (day_count.where(day_count['location']==location).dropna()).iterrows():
+		day_count.at[index,['day count']] = i
+		i = i+1
+i=0
+'''
+for location in day_count.drop_duplicates('location')['location']:
+	fig, axs = plt.subplots(2)
+	axs[0].plot(day_count['day count'].where(day_count['location'] == location).dropna() ,day_count['precent of population'].where(day_count['location'] == location).dropna(),linewidth=2.0,label = location , linestyle = "-", color = "#FF0000")
+	axs[1].plot(day_count['day count'].where(day_count['location'] == location).dropna() ,day_count['new cases of population'].where(day_count['location'] == location).dropna(),linewidth=2.0,label = location , linestyle = "-", color = "#4400FF")
+	axs[0].legend()
+	axs[1].legend()
+	fig.savefig('./countrys/'+location+'.png', bbox_inches='tight')
+'''
+######
+#Mechine learning
+######
+for location in day_count['location'].drop_duplicates():
+	i = i+1
+	for index, row in (day_count.where(day_count['location']==location).dropna()).iterrows():
+		day_count.at[index,['location']] = i
+day_count['location'] = day_count['location'].astype('int')
+
+bins = (2,6.5,8)
+day_count['precent of population'] = pd.cut(day_count['precent of population'],bins)
+X = day_count.where(day_count['location']==19).dropna().drop("precent of population",axis = 1)
+y = day_count.where(day_count['location']==19).dropna()['precent of population']
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+lgr = LogisticRegression()
+lgr.fit(X_train,y_train)
+predict_lgr = lgr.predict(X_test)
+print(predict_lgr)
